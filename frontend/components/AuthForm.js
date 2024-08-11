cc15323d2eb7cfd068c4fae63ebaa0bcc0b624cd
David S. <neojohn2312@gmail.com>
2024-08-11 18:43:32 -0500

    1st-commit

--- frontend/components/AuthForm.js
+++ frontend/components/AuthForm.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function AuthForm() {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState("");
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value)
@@ -11,13 +14,27 @@ export default function AuthForm() {
     setPassword(event.target.value)
   }
   const handleSubmit = async (event) => {
-
+    event.preventDefault()
+    setError("")
+    try {
+      throw new Error("ugghh")
+    } catch (err) {
+      setError(
+        err?.response?.data?.message ||
+        "An error occurred. Please try again"
+      )
+    }
+    // POST username and password to the login endpoint
+    // if fail, set error message in state so it renders
+    // If OK
+    // 1- save tokens to browser's local storage
+    // 2- navigate user to the /cereals route
   }
 
   return (
     <div className="container">
       <div aria-live="polite"></div>
-      <div aria-live="assertive" style={{ color: 'red' }}></div>
+      <div aria-live="assertive" style={{ color: 'red' }}>{error}</div>
       <h3>Login Form</h3>
       <form onSubmit={handleSubmit}>
         <div>
